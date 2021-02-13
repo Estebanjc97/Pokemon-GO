2b792ffd729b379d61b54adc593a4bff1f29336b
DanielSR1999 <daniel.sanchezramirez02@gmail.com>
2021-02-12 23:23:39 -0500

    Vuforia plugin imported
    Intenté realizar el AR con ARCore pero la exportacion generaba errores de incompatibilidad del plugin de ARCore con versionamiento de Unity y SDK Android

--- Pokemon GO/Assets/Scripts/AuthenticationManager.cs
+++ Pokemon GO/Assets/Scripts/AuthenticationManager.cs
@@ -96,9 +96,6 @@ public class AuthenticationManager : MonoBehaviour
             string name = user.DisplayName;
             string email = user.Email;
             System.Uri photo_url = user.PhotoUrl;
-            // The user's Id, unique to the Firebase project.
-            // Do NOT use this value to authenticate with your backend server, if you
-            // have one; use User.TokenAsync() instead.
             string uid = user.UserId;
         }
     }
