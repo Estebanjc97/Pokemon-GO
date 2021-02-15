23bfbc12fcf0a9e5891416d2a3898102d70e6c33
DanielSR1999 <daniel.sanchezramirez02@gmail.com>
2021-02-15 01:59:00 -0500

    Interactive pokedex showing info

---
+++ Pokemon GO/Assets/PokedexController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokedexController : MonoBehaviour
+{
+    [SerializeField]
+    public List<PokeAPIController> pokeAPIControllers;
+    public string pokemonButtonsTag = "Pokemon";
+
+    [Tooltip("Ingresa el número mayor que deseas agregar a la pokedex")]
+    public int pokedexMaxValue = 800;
+
+    [Tooltip("Númer de elementos que se visualizarán en pantalla")]
+    public int elementsInTable = 10;
+    private void Start()
+    {
+        GameObject[] pokemonButtons=GameObject.FindGameObjectsWithTag(pokemonButtonsTag); //creamos un array con los 10 botones que contienen la informacion de los pokemons
+
+        for (int i = 0; i < pokemonButtons.Length; i++)
+        {
+            pokeAPIControllers.Add(pokemonButtons[i].GetComponent<PokeAPIController>()); //creamos una lista de las clases que contienen dichos botones
+        }
+        elementsInTable = pokemonButtons.Length;
+    }
+    public void NextPokemons()
+    {
+        foreach(PokeAPIController _pokeAPIController in pokeAPIControllers)
+        {
+            if(_pokeAPIController.pokemonID<=(pokedexMaxValue-elementsInTable)) //Verificamos que el identificador no vaya a superar el valor maximo de la pokedex luego de realizar el incremento
+            {
+                _pokeAPIController.pokemonID += 10;
+            }
+            else
+            {
+                _pokeAPIController.pokemonID -= (pokedexMaxValue - elementsInTable); //si hemos llegado al final de la pokedex y presionamos para ver la siguiente lista volveremos al inicio
+            }
+            _pokeAPIController.CallGetPokemonAtIndex();
+        }
+    }
+
+    public void PreviousPokemons()
+    {
+        foreach (PokeAPIController _pokeAPIController in pokeAPIControllers)
+        {
+            if (_pokeAPIController.pokemonID >elementsInTable) //Verificamos que el identificador no vaya a superar el valor maximo de la pokedex luego de realizar el incremento
+            {
+                _pokeAPIController.pokemonID -= 10;
+            }
+            else
+            {
+                _pokeAPIController.pokemonID += (pokedexMaxValue - elementsInTable); //si hemos llegado al inicio de la pokedex y presionamos para ver la anterior lista iremos al final
+            }
+            _pokeAPIController.CallGetPokemonAtIndex();
+        }
+    }
+}
---
+++ Pokemon GO/Assets/Scripts/PokeAPIController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+using UnityEngine.UI;
+
+public class PokeAPIController : MonoBehaviour
+{
+    public int pokemonID = 1;
+    public RawImage pokeRawImage;
+    public Text pokeNameText, pokeNumText;
+    string[] pokeTypeNames;
+    public Text[] pokeTypeTextArray;
+    [SerializeField]
+
+    private readonly string basePokeURL = "https://pokeapi.co/api/v2/";
+    UnityWebRequest pokeSpriteRequest, pokeInfoRequest;
+
+    private void Start()
+    {
+        CallGetPokemonAtIndex();
+    }
+    
+    public void CallGetPokemonAtIndex()
+    {
+        StartCoroutine(GetPokemonAtIndex(pokemonID));
+    }
+    IEnumerator GetPokemonAtIndex(int pokemonIndex)
+    {
+        // Establecemos la URL correspondiente al identificador del pokemon asignado
+
+        string pokemonURL = basePokeURL + "pokemon/" + pokemonIndex.ToString();
+        // Example URL: https://pokeapi.co/api/v2/pokemon/151
+
+        pokeInfoRequest = UnityWebRequest.Get(pokemonURL);
+
+        yield return pokeInfoRequest.SendWebRequest();
+
+        if (pokeInfoRequest.isNetworkError || pokeInfoRequest.isHttpError)
+        {
+            Debug.LogError(pokeInfoRequest.error);
+            yield break;
+        }
+
+        JSONNode pokeInfo = JSON.Parse(pokeInfoRequest.downloadHandler.text);
+
+        string pokeName = pokeInfo["name"];
+        string pokeSpriteURL = pokeInfo["sprites"]["front_default"];
+
+        JSONNode pokeTypes = pokeInfo["types"];
+        pokeTypeNames = new string[pokeTypes.Count];
+
+        for (int i = 0, j = pokeTypes.Count - 1; i < pokeTypes.Count; i++, j--)
+        {
+            pokeTypeNames[j] = pokeTypes[i]["type"]["name"];
+        }
+
+        // Conseguimos el sprite del pokemon
+
+        pokeSpriteRequest = UnityWebRequestTexture.GetTexture(pokeSpriteURL);
+
+        yield return pokeSpriteRequest.SendWebRequest();
+
+        if (pokeSpriteRequest.isNetworkError || pokeSpriteRequest.isHttpError)
+        {
+            Debug.LogError(pokeSpriteRequest.error);
+            yield break;
+        }
+
+        // Establecemos los valores de la UI
+
+        pokeNameText.text = pokeName;
+        pokeNumText.text = "#" + pokemonID;
+
+
+    }
+    public void UpdatePokemonInfo()
+    {
+        pokeRawImage.texture = DownloadHandlerTexture.GetContent(pokeSpriteRequest);
+
+
+        for (int i = 0; i < pokeTypeNames.Length; i++)
+        {
+            pokeTypeTextArray[i].text = pokeTypeNames[i];
+        }
+    }
+}
