8617be678cdf6efa86748c8af0ac97879e2b8784
DanielSR1999 <daniel.sanchezramirez02@gmail.com>
2021-02-16 20:21:30 -0500

    search bar and full pokemon info in pokedex

--- Pokemon GO/Assets/Scripts/PokeAPIController.cs
+++ Pokemon GO/Assets/Scripts/PokeAPIController.cs
@@ -11,8 +11,17 @@ public class PokeAPIController : MonoBehaviour
     public RawImage pokeRawImage;
     public Text pokeNameText, pokeNumText;
     string[] pokeTypeNames;
+    string[] pokemonAbilities;
+    string pokeName;
     public Text[] pokeTypeTextArray;
-    [SerializeField]
+    public Text[] pokemonAbilitiesTextArray;
+    public Text pokeNameTextExpand;
+    public Text pokemonIDExpand;
+    public Text pokemonBaseXP;
+
+    public Text pokemonHeightText;
+    string pokemonHeightString;
+    string pokBaseXP;
 
     private readonly string basePokeURL = "https://pokeapi.co/api/v2/";
     UnityWebRequest pokeSpriteRequest, pokeInfoRequest;
@@ -45,17 +54,28 @@ public class PokeAPIController : MonoBehaviour
 
         JSONNode pokeInfo = JSON.Parse(pokeInfoRequest.downloadHandler.text);
 
-        string pokeName = pokeInfo["name"];
+        pokeName = pokeInfo["name"];
         string pokeSpriteURL = pokeInfo["sprites"]["front_default"];
 
+        pokemonHeightString = pokeInfo["height"];
+        pokBaseXP = pokeInfo["base_experience"];
+
         JSONNode pokeTypes = pokeInfo["types"];
+        JSONNode pokeAbilities = pokeInfo["abilities"];
+
         pokeTypeNames = new string[pokeTypes.Count];
+        pokemonAbilities = new string[pokeAbilities.Count];
 
         for (int i = 0, j = pokeTypes.Count - 1; i < pokeTypes.Count; i++, j--)
         {
             pokeTypeNames[j] = pokeTypes[i]["type"]["name"];
         }
+        for (int i = 0, j = pokeAbilities.Count - 1; i < pokeAbilities.Count; i++, j--)
+        {
+            pokemonAbilities[j] = pokeAbilities[i]["ability"]["name"];
+        }
 
+        
         // Conseguimos el sprite del pokemon
 
         pokeSpriteRequest = UnityWebRequestTexture.GetTexture(pokeSpriteURL);
@@ -72,17 +92,34 @@ public class PokeAPIController : MonoBehaviour
 
         pokeNameText.text = pokeName;
         pokeNumText.text = "#" + pokemonID;
-
-
     }
+    
     public void UpdatePokemonInfo()
     {
         pokeRawImage.texture = DownloadHandlerTexture.GetContent(pokeSpriteRequest);
+        pokemonHeightText.text = pokemonHeightString;
+        pokeNameTextExpand.text = pokeName;
+        pokemonIDExpand.text = pokemonID.ToString();
+        pokemonBaseXP.text = pokBaseXP;
 
+        foreach (Text poketypes in pokeTypeTextArray)
+        {
+            poketypes.text = "";
+        }
+
+        foreach(Text pokeabilities in pokemonAbilitiesTextArray)
+        {
+            pokeabilities.text = "x";
+        }
 
         for (int i = 0; i < pokeTypeNames.Length; i++)
         {
             pokeTypeTextArray[i].text = pokeTypeNames[i];
         }
+        for (int i = 0; i < pokemonAbilities.Length; i++)
+        {
+            pokemonAbilitiesTextArray[i].text = pokemonAbilities[i];
+        }  
     }
+
 }
--- Pokemon GO/Assets/Scripts/PokedexController.cs
+++ Pokemon GO/Assets/Scripts/PokedexController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using SimpleJSON;
 
 public class PokedexController : MonoBehaviour
 {
@@ -14,6 +17,115 @@ public class PokedexController : MonoBehaviour
     [Tooltip("Númer de elementos que se visualizarán en pantalla")]
     public int elementsInTable = 10;
     int getPokeAPIController = 0; //variable para que la asignación de la clase solo se haga por primera vez
+
+    UnityWebRequest pokemonInfo, pokeSpriteRequest;
+    private readonly string basePokeURL = "https://pokeapi.co/api/v2/";
+    public Text errorMessageText;
+    string pokeName;
+    string pokemonHeightString;
+    string pokBaseXP;
+    string pokeID;
+    string[] pokeTypeNames;
+    string[] pokemonAbilities;
+    public Text[] pokeTypeTextArray;
+    public Text[] pokemonAbilitiesTextArray;
+
+    public Text pokemonHeightText;
+    public Text pokeNameTextExpand;
+    public Text pokemonIDExpand;
+    public Text pokemonBaseXP;
+
+    public RawImage pokeRawImage;
+
+    private void Start()
+    {
+        StartCoroutine(TryGetPokemonByName("bulbasaur"));
+    }
+    public void SearchPokemon(InputField searchInputField)
+    {
+        StartCoroutine(TryGetPokemonByName(searchInputField.text));
+    }
+    IEnumerator TryGetPokemonByName(string name) //esta función es casi la misma que la corroutina que está en PokeAPIController con la diferencia que aqui se realiza la búsqueda por nombre y no por id
+    {
+        string pokemonURL = basePokeURL + "pokemon/" + name.ToLower();
+        pokemonInfo = UnityWebRequest.Get(pokemonURL);
+        yield return pokemonInfo.SendWebRequest();
+
+        if (pokemonInfo.isNetworkError || pokemonInfo.isHttpError)
+        {
+            errorMessageText.text = "Pokemon no encontrado";
+            yield break;
+        }
+        else
+        {
+            errorMessageText.text = "";
+        }
+
+        JSONNode pokeInfo = JSON.Parse(pokemonInfo.downloadHandler.text);
+
+        pokeName = pokeInfo["name"];
+        string pokeSpriteURL = pokeInfo["sprites"]["front_default"];
+
+        pokemonHeightString = pokeInfo["height"];
+        pokBaseXP = pokeInfo["base_experience"];
+        pokeID = pokeInfo["id"];
+
+        JSONNode pokeTypes = pokeInfo["types"];
+        JSONNode pokeAbilities = pokeInfo["abilities"];
+
+        pokeTypeNames = new string[pokeTypes.Count];
+        pokemonAbilities = new string[pokeAbilities.Count];
+
+        for (int i = 0, j = pokeTypes.Count - 1; i < pokeTypes.Count; i++, j--)
+        {
+            pokeTypeNames[j] = pokeTypes[i]["type"]["name"];
+        }
+        for (int i = 0, j = pokeAbilities.Count - 1; i < pokeAbilities.Count; i++, j--)
+        {
+            pokemonAbilities[j] = pokeAbilities[i]["ability"]["name"];
+        }
+
+
+        pokeSpriteRequest = UnityWebRequestTexture.GetTexture(pokeSpriteURL);
+
+        yield return pokeSpriteRequest.SendWebRequest();
+
+        if (pokeSpriteRequest.isNetworkError || pokeSpriteRequest.isHttpError)
+        {
+            Debug.LogError(pokeSpriteRequest.error);
+            yield break;
+        }
+
+
+        UpdatePokemonInfo();
+    }
+    public void UpdatePokemonInfo()
+    {
+        pokeRawImage.texture = DownloadHandlerTexture.GetContent(pokeSpriteRequest);
+        pokemonHeightText.text = pokemonHeightString;
+        pokeNameTextExpand.text = pokeName;
+        pokemonIDExpand.text = pokeID;
+        pokemonBaseXP.text = pokBaseXP;
+
+        foreach (Text poketypes in pokeTypeTextArray)
+        {
+            poketypes.text = "";
+        }
+
+        foreach (Text pokeabilities in pokemonAbilitiesTextArray)
+        {
+            pokeabilities.text = "x";
+        }
+
+        for (int i = 0; i < pokeTypeNames.Length; i++)
+        {
+            pokeTypeTextArray[i].text = pokeTypeNames[i];
+        }
+        for (int i = 0; i < pokemonAbilities.Length; i++)
+        {
+            pokemonAbilitiesTextArray[i].text = pokemonAbilities[i];
+        }
+    }
     public void GetPokeAPIControllers()
     {
         if(getPokeAPIController==0)
